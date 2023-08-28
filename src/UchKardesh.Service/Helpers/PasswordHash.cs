1b329aaa20287cb22978181850791429c0a0df2c
iskandar kodirov <eiscocyber@gmail.com>
2023-08-28 05:17:37 +0500

    Helpers and extensions added

---
+++ src/UchKardesh.Service/Helpers/PasswordHash.cs
@@ -0,0 +1,15 @@
+﻿namespace UchKardesh.Service.Helpers;
+
+public static class PasswordHash
+{
+	public static string Hash(this string password)
+	{
+		password = BCrypt.Net.BCrypt.HashPassword(password);
+		return password;
+	}
+
+	public static bool Verify(this string hashedPassword, string password)
+	{
+		return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+	}
+}
---
+++ src/UchKardesh.Service/Helpers/PathHelper.cs
@@ -0,0 +1,6 @@
+﻿namespace UchKardesh.Service.Helpers;
+
+public static class PathHelper
+{
+    public static string WebRootPath { get; set; }
+}
