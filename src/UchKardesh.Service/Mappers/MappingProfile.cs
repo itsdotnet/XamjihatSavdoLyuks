0cf53239f714f1233b3c7b807ad14fd00200906f
iskandar kodirov <eiscocyber@gmail.com>
2023-09-11 12:28:51 +0500

    Mapping profile added

--- src/UchKardesh.Service/Mappers/MappingProfile.cs
+++ src/UchKardesh.Service/Mappers/MappingProfile.cs
@@ -3,6 +3,7 @@ using UchKardesh.Domain.Entities;
 using UchKardesh.Service.DTOs.Attachments;
 using UchKardesh.Service.DTOs.Categories;
 using UchKardesh.Service.DTOs.Products;
+using UchKardesh.Service.DTOs.Sessions;
 using UchKardesh.Service.DTOs.Users;
 
 namespace UchKardesh.Service.Mappers;
@@ -11,29 +12,46 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        //user
+        #region User
 
         CreateMap<UserCreationDto, User>().ReverseMap();
         CreateMap<UserUpdateDto, User>().ReverseMap();
         CreateMap<User, UserResultDto>().ReverseMap();
         CreateMap<UserUpdateDto, UserResultDto>().ReverseMap();
 
-        //product
+        #endregion
+
+        #region Product
 
         CreateMap<ProductCreationDto, Product>().ReverseMap();
         CreateMap<ProductUpdateDto, Product>().ReverseMap();
         CreateMap<Product, ProductResultDto>().ReverseMap();
         CreateMap<ProductResultDto, ProductUpdateDto>().ReverseMap();
 
-        //category
+        #endregion
+
+        #region Category
 
         CreateMap<CategoryCreationDto, Category>().ReverseMap();
         CreateMap<CategoryUpdateDto, Category>().ReverseMap();
         CreateMap<Category, CategoryResultDto>().ReverseMap();
         CreateMap<CategoryResultDto, CategoryUpdateDto>().ReverseMap();
 
-        //attachment
+        #endregion
+
+        #region Session
+        
+        CreateMap<SessionCreationDto, Session>().ReverseMap();
+        CreateMap<SessionUpdateDto, Session>().ReverseMap();
+        CreateMap<Session, SessionResultDto>().ReverseMap();
+        CreateMap<SessionResultDto, SessionUpdateDto>().ReverseMap();
+
+        #endregion
+
+        #region Attachment
 
         CreateMap<Attachment, AttachmentResultDto>().ReverseMap();
+
+        #endregion
     }
 }
