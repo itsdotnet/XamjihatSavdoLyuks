7699b2513f4b7c3586a1b215107f668ada2ce583
iskandar kodirov <eiscocyber@gmail.com>
2023-09-11 12:31:52 +0500

    UserService interface added

---
+++ src/UchKardesh.Service/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using UchKardesh.Service.DTOs.Users;
+
+namespace UchKardesh.Service.Interfaces;
+
+public interface IUserService
+{
+    Task<bool> DeleteAsync(long id);
+    Task<UserResultDto> GetByIdAsync(long id);
+    Task<IEnumerable<UserResultDto>> GetAllAsync();
+    Task<UserResultDto> ModifyAsync(UserUpdateDto dto);
+    Task<UserResultDto> CreateAsync(UserCreationDto dto);
+    Task<IEnumerable<UserResultDto>> GetByName(string name);
+    Task<bool> CheckUserAsync(string firstname, string password);
+    Task<UserResultDto> ModifyPasswordAsync(long id, string oldPass, string newPass);
+}
